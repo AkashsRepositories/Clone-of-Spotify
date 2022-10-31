f59c0a13f30c01a78ade53a17fce5f95b3f9780b
sky <aakashjangra2002@gmail.com>
2022-10-31 07:36:38 +0530

    feat: playlists structure and content coming with js

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -34,30 +34,60 @@ const onPlaylistClick = (event) => {
     console.log(event.target)
 }
 
-const loadFeaturedPlaylist  = async () => {
-    const {playlists: {items}} = await fetchRequest(ENDPOINT.featuredPlaylist);
-    console.log(items)
-    const playlistContainer = document.querySelector('#featured-playlist-items');
+const loadPlaylist  = async (endpoint, elementId) => {
+    const {playlists: {items}} = await fetchRequest(endpoint);
+    const playlistContainer = document.querySelector(`#${elementId}`);
 
     for(let {name, id, description, images:[{url}]} of items){
         const currentSection = document.createElement("section");
-        currentSection.className= "rounded border-2 p-4 hover:cursor-pointer";
+        currentSection.className= "rounded p-4 hover:cursor-pointer hover:bg-light-black";
         currentSection.id = id;
         //adding click event on playlist card
         currentSection.addEventListener('click', onPlaylistClick);
         currentSection.innerHTML = `
                         <img class="rounded object-contain mb-2 shadow" src="${url}" alt="${name} cover image">
-                        <h2 class="text-sm">${name}</h2>
-                        <h3 class="text-xs">${description}</h3>
+                        <h2 class="text-base font-semibold mb-4 truncate">${name}</h2>
+                        <h3 class="text-xs text-secondary line-clamp-2">${description}</h3>
                         `
         playlistContainer.appendChild(currentSection);
     }
 
 }
 
+function loadPlaylists(){
+    //featured playlist   
+    loadPlaylist(ENDPOINT.featuredPlaylist, 'featured-playlist-items');
+    //top playlists
+    loadPlaylist(ENDPOINT.toplists, 'top-playlist-items');
+}
+
+//for generating playlists' structure
+const fillContentInDashboard = () => {
+    const playlistArr = [["featured", "featured-playlist-items"], ["top playlists", "top-playlist-items"]];
+    const playlistsContainer = document.querySelector('#page-content');
+    let innerHTML = '';
+    for(let [type, id] of playlistArr){
+        innerHTML += `
+            <article class="p-4">
+                <h1 class="text-2xl mb-4">${type}</h1>
+                <section id="${id}" class="${type}-songs grid grid-cols-auto-fill-cards gap-4">
+                    <!-- ${type} playlists here - generated via js -->
+                </section>
+            </article>
+        `;
+    }
+
+    playlistsContainer.innerHTML = innerHTML;
+}
+
+//on loading on document - automatically exected when respective html file loads up in browser
 document.addEventListener('DOMContentLoaded', () => {
-    loadUserProfile();    
-    loadFeaturedPlaylist();
+    //user's profile name and image
+    loadUserProfile(); 
+    //fill content in dashboard
+    fillContentInDashboard();
+    //loading playlists
+    loadPlaylists();
     document.addEventListener('click',  () => {
         const profileMenu = document.querySelector("#profile-menu");
         if(!profileMenu.classList.contains("hidden")) {
